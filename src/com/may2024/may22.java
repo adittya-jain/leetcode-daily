122935565f13034280b52be3bb8fe8dd7ba4bff0
Aditya Jain <jain.aditya1510@gmail.com>
2024-05-23 01:34:49 +0530

    may22 done

---
+++ src/com/may2024/may22.java
@@ -0,0 +1,39 @@
+package com.may2024;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class may22 {
+}
+class SolutionMay22 {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (isPalindrome(s, start, end - 1)) {
+                path.add(s.substring(start, end));
+                // Recur to find other partitions
+                backtrack(s, end, path, result);
+                // Backtrack to explore other partitions
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
