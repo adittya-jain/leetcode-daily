2879dbd1a75ad69bb9775264fdbb8744e47e8a85
Aditya Jain <jain.aditya1510@gmail.com>
2024-05-30 23:54:12 +0530

    may29 done

---
+++ src/com/may2024/may30.java
@@ -0,0 +1,29 @@
+package com.may2024;
+
+public class may30 {
+}
+class SolutionMay30 {
+    public int countTriplets(int[] arr) {
+        int count = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int a = 0;
+                for (int k = i; k < j; k++) {
+                    a ^= arr[k];
+                }
+
+                int b = 0;
+                for (int k = j; k < n; k++) {
+                    b ^= arr[k];
+                    if (a == b) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
